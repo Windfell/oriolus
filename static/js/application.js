8f97075710b03a6ad3e79172f01c39e7175bc4f6
Brooke Lee <ssmart1314@yeah.net>
2013-04-24 12:00:26 +0800

    css

--- static/js/application.js
+++ static/js/application.js
@@ -6,6 +6,20 @@ $(document).ready(function () {
         id = $("#edit_blog_id").val()
 //        location.href = "/blog/edit/" + id;
     });
-
+    $(".autotext").focus(function () {
+        if (this.value = "标签用英文逗号(,)分隔") {
+            this.value = "";
+        }
+    });
 });
+
+function replaceDot(str) {
+    var Obj = document.getElementById(str);
+    var oldValue = Obj.value;
+    while (oldValue.indexOf("，") != -1)//寻找每一个中文逗号，并替换
+    {
+        Obj.value = oldValue.replace("，", ",");
+        oldValue = Obj.value;
+    }
+}
 
